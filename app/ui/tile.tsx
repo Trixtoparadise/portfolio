520afd0842c410072f2d09dc1141380777a60897
Trixtoparadise <hlamolo.mhlanga7@gmail.com>
2026-07-14 20:57:14 +0200

    tile component

--- app/ui/tile.tsx
+++ app/ui/tile.tsx
@@ -1,5 +1,8 @@
 "use client"
 import * as React from 'react';
+import Github from "@mui/icons-material/GitHub";
+import EmailOutlined from "@mui/icons-material/EmailOutlined";
+import LocalPhoneOutLined from "@mui/icons-material/LocalPhoneOutlined";
 import { ButtonBase, Stack } from "@mui/material";
 
 type PropType = {
@@ -10,30 +13,70 @@ type PropType = {
     data: string[];
 }
 
+const designPalette: Record<string, string> = {
+    info: 'bg-[#E54848]!',
+    interests: 'bg-[#325FBE]!',
+    skills: 'bg-[#FCA311]!',
+    projects: 'bg-[#53A548]!',
+    contact: 'bg-[#5928AC]!',
+    experience: 'bg-[#EC368D]!',
+    education: 'bg-[#F26419]!',
+};
+
+
 export default function Tile(props : PropType) {
     const { title, subTitle, background, height, data } = props;
     const [flip, setFlip] = React.useState(false);
+    const bgColor = designPalette[background] || 'bg-gray-100';
 
     return flip ? (
         <Stack 
             style={{ flexGrow: height, flexBasis: 0 }}
-            className={`shadow-xl/40 rounded-md bg-${background}!`}
+            className={`${bgColor} shadow-xl/40 rounded-md!`}
         >
             <ButtonBase
                 onClick={() => setFlip(false)} 
-                className={`flex grow py-2.5! px-6! overflow-hidden`}    
+                className={`flex grow py-2.5! px-6! overflow-hidden ${bgColor} rounded-md!`}    
             >
-                <Stack className="text-center! text-primary! mx-6!">
-                    <ul>
-                        {data.map((item, index) => <li key={index} className="font-light font-urbanist! text-[1.2rem] list-disc">{item}</li> )}
-                    </ul>
-                </Stack>
+                {background == "contact" ? (
+                    <Stack className="text-primary! mx-6!">
+                        <ul className='text-center! list-inside!'>
+                            <Stack direction="row" className='justify-around! mx-20 '>
+                                <li className="font-light font-urbanist! text-[1.2rem] py-4 "> 
+                                    <a href="mailto:hlamolo.mhlanga7@gmail.com" onClick={(e) => e.stopPropagation()}>
+                                        <EmailOutlined className={`h-8! w-8! text-primary! hover:text-gray-200 transition-colors duration-200 focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80`} />
+                                    </a>
+                                </li>
+                                <li className="font-light font-urbanist! text-[1.2rem] py-4"> 
+                                    <a href="https://github.com/Trixtoparadise" target="_blank" rel="noreferrer" onClick={(e) => e.stopPropagation()}>
+                                        <Github className={`h-8! w-8! text-primary! hover:text-gray-200 transition-colors duration-200 focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80`} />
+                                    </a>
+                                </li>
+                                <li className="font-light font-urbanist! text-[1.2rem] py-4"> 
+                                    <a href="tel:+27783482171" onClick={(e) => e.stopPropagation()}>
+                                        <LocalPhoneOutLined className={`h-8! w-8! text-primary! hover:text-gray-200 transition-colors duration-200 focus:outline-none focus-visible:underline focus-visible:underline-offset-8 focus-visible:decoration-accent/80`} />
+                                    </a>
+                                </li>
+                            </Stack>
+                        </ul>
+                        <ul className='text-center! list-inside!'>
+                            {data.map((item, index) => <li key={index}  className="font-light font-urbanist! text-[1.2rem] py-4" dangerouslySetInnerHTML={{ __html: item }}/>)}
+                        </ul>
+                    </Stack>
+                ) : (
+                    
+                    <Stack className="text-primary! mx-6!">
+                        <ul className='text-center! list-inside!'>
+                            {data.map((item, index) => <li key={index}  className="font-light font-urbanist! text-[1.2rem] py-4 list-disc" dangerouslySetInnerHTML={{ __html: item }}/>)}
+                        </ul>
+                    </Stack>
+                )}
             </ButtonBase>
         </Stack>
     ) : (
         <Stack 
             style={{ flexGrow: height, flexBasis: 0 }}
-            className={`shadow-xl/40 rounded-md bg-${background}!`}
+            className={`shadow-xl/40 rounded-md ${bgColor}`}
         >
             <ButtonBase
                 onClick={() => setFlip(true)} 
