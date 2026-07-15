4247b6c989d419f7baf9e084cc87012e32e7e4d0
Trixtoparadise <hlamolo.mhlanga7@gmail.com>
2026-07-15 20:57:13 +0200

    adding scroll for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 			lang="en"
 			className={`${annieUseYourTelescope.variable} ${urbanist.className} h-full antialiased`}
 		>
-			<body className="h-screen! overflow-hidden! w-full! flex flex-col bg-background!">{children}</body>
+			<body className="h-auto! md:h-screen! lg:h-screen! overflow-scroll! md:overflow-hidden! lg:overflow-hidden! w-full! flex! flex-col! bg-background!">{children}</body>
 		</html>
 	);
 }
