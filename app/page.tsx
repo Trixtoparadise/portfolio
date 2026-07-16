3080b8c77d7807b36fe9ed7ce973629728dac0e4
Trixtoparadise <hlamolo.mhlanga7@gmail.com>
2026-07-16 23:14:48 +0200

    addiing numbers and paper texture

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,8 @@ export default function Home() {
                             title={`Hlamolo Mhlanga`}
                             subTitle="Web and Mobile Application Developer."
                             background="info"
-                            height='h-200! md:grow-[4.5] lg:grow-[4.5]'
+                            height='grow-[4.5]'
+                            number='1'
                             data={['<b><b>Age and Gender:</b></b> 25 years old , Male' , '<b><b>Address:</b></b> House No - 987, Mandlethu, 0458, Mpumalanga.']}
                         />
                         <Tile 
@@ -32,6 +33,7 @@ export default function Home() {
                             subTitle="What I am passionate about and what I would like to learn."
                             background="interests"
                             height='grow-[5.5]'
+                            number='3'
                             data={["Studying <b><b>design</b></b>, the foundation of every system known to mankind and applying the lessons on my <b><b>applications</b></b>, studying the balance relationship between <b><b>form</b></b> and <b><b>function</b></b>, and how it affects the <b><b>user experience</b></b> and <b><b>interaction</b></b>."]}
                         />
                     </Stack>
@@ -44,6 +46,7 @@ export default function Home() {
                             subTitle="Languages, Frameworks, Stacks, databases."
                             background="skills"
                             height='grow-[6.5]'
+                            number='2'
                             data={['<b><b>Languages:</b></b> C#, C++, Javascript, Typescript.', '<b><b>Backend Frameworks:</b></b> Node.js (Express), ASP.NET Core WebAPI.', '<b><b>Frontend Frameworks:</b></b> React.js, React Native, .NET MAUI', '<b><b>Databases:</b></b>  PostgresSQL, MongoDB Atlas.', '<b><b>Cloud Platforms:</b></b>  Amazon Web Services RDB, Microsoft Azure ASP NET Core Web API, Vercel Next.js application.']}
                         />
                         <Tile 
@@ -51,6 +54,7 @@ export default function Home() {
                             subTitle="Applications, Databases, Demos, Websites."
                             background="projects"
                             height='grow-[3.5]'
+                            number='4'
                             data={['<b><b>www.mooseum.online</b></b>', '<b><b>Inventori</b></b> (Link to source code)', '<b><b>Odyssey</b></b> (link to source code)']}
                         />
                     </Stack>
@@ -63,6 +67,7 @@ export default function Home() {
                             subTitle="For an Interview, discussion or collaboration."
                             background="contact"
                             height='grow-2'
+                            number='7'
                             data={['Choose how you want to connect.']}
                         />
                         <Tile 
@@ -70,6 +75,7 @@ export default function Home() {
                             subTitle="Where I’ve worked and what I did there."
                             background="experience"
                             height='grow-5'
+                            number='6'
                             data={['I worked at <b><b>Nkangala District Municipality</b></b> from July 2024 - June 2026 as an <b><b>Information and Communication Technology</b></b> intern. Rotating in different roles within the department as a service desk operator, assistant to the municipality technician or a reporting analyst.']}
                         />
                         <Tile 
@@ -77,6 +83,7 @@ export default function Home() {
                             subTitle="High school & University."
                             background="education"
                             height='grow-3'
+                            number='5'
                             data={['I studied at <b><b>Phumzile Secondary School</b></b> and earned a <b><b>National Senior Certificate</b></b> in January 2018.', 'I went on to study <b><b>Computer Science and Applied Mathematics</b></b> at <b><b>Wits University</b></b> and earned a Bachelor of Science in April 2023.']}
                         />
                     </Stack>
@@ -95,6 +102,7 @@ export default function Home() {
                             subTitle="Web and Mobile Application Developer."
                             background="info"
                             height='grow-[2.25]'
+                            number='1'
                             data={['<b><b>Age and Gender:</b></b> 25 years old , Male' , '<b><b>Address:</b></b> House No - 987, Mandlethu, 0458, Mpumalanga.']}
                         />
                         <Tile 
@@ -102,6 +110,7 @@ export default function Home() {
                             subTitle="What I am passionate about and what I would like to learn."
                             background="interests"
                             height='grow-3'
+                            number='3'
                             data={["Studying <b><b>design</b></b>, the foundation of every system known to mankind and applying the lessons on my <b><b>applications</b></b>, studying the balance relationship between <b><b>form</b></b> and <b><b>function</b></b>, and how it affects the <b><b>user experience</b></b> and <b><b>interaction</b></b>."]}
                         />
                         <Tile 
@@ -109,6 +118,7 @@ export default function Home() {
                             subTitle="Where I’ve worked and what I did there."
                             background="experience"
                             height='grow-[2.75]'
+                            number='6'
                             data={['I worked at <b><b>Nkangala District Municipality</b></b> from July 2024 - June 2026 as an <b><b>Information and Communication Technology</b></b> intern. Rotating in different roles within the department as a service desk operator, assistant to the municipality technician or a reporting analyst.']}
                         />
                         <Tile 
@@ -116,6 +126,7 @@ export default function Home() {
                             subTitle="For an Interview, discussion or collaboration."
                             background="contact"
                             height='grow-2'
+                            number='7'
                             data={['Choose how you want to connect.']}
                         />
                     </Stack>
@@ -128,6 +139,7 @@ export default function Home() {
                             subTitle="Languages, Frameworks, Stacks, databases."
                             background="skills"
                             height='grow-5'
+                            number='2'
                             data={['<b><b>Languages:</b></b> C#, C++, Javascript, Typescript.', '<b><b>Backend Frameworks:</b></b> Node.js (Express), ASP.NET Core WebAPI.', '<b><b>Frontend Frameworks:</b></b> React.js, React Native, .NET MAUI', '<b><b>Databases:</b></b>  PostgresSQL, MongoDB Atlas.', '<b><b>Cloud Platforms:</b></b>  Amazon Web Services RDB, Microsoft Azure ASP NET Core Web API, Vercel Next.js application.']}
                         />
                         <Tile 
@@ -135,6 +147,7 @@ export default function Home() {
                             subTitle="Applications, Databases, Demos, Websites."
                             background="projects"
                             height='grow-2'
+                            number='4'
                             data={['<b><b>www.mooseum.online</b></b>', '<b><b>Inventori</b></b> (Link to source code)', '<b><b>Odyssey</b></b> (link to source code)']}
                         />
                         <Tile 
@@ -142,6 +155,7 @@ export default function Home() {
                             subTitle="High school & University."
                             background="education"
                             height='grow-3'
+                            number='5'
                             data={['I studied at <b><b>Phumzile Secondary School</b></b> and earned a <b><b>National Senior Certificate</b></b> in January 2018.', 'I went on to study <b><b>Computer Science and Applied Mathematics</b></b> at <b><b>Wits University</b></b> and earned a Bachelor of Science in April 2023.']}
                         />
                     </Stack>
@@ -159,6 +173,7 @@ export default function Home() {
                             subTitle="Web and Mobile Application Developer."
                             background="info"
                             height='h-75'
+                            number='1'
                             data={['<b><b>Age and Gender:</b></b> 25 years old , Male' , '<b><b>Address:</b></b> House No - 987, Mandlethu, 0458, Mpumalanga.']}
                         />
                         <Tile 
@@ -166,6 +181,7 @@ export default function Home() {
                             subTitle="What I am passionate about and what I would like to learn."
                             background="interests"
                             height='h-95'
+                            number='2'
                             data={["Studying <b><b>design</b></b>, the foundation of every system known to mankind and applying the lessons on my <b><b>applications</b></b>, studying the balance relationship between <b><b>form</b></b> and <b><b>function</b></b>, and how it affects the <b><b>user experience</b></b> and <b><b>interaction</b></b>."]}
                         />
                         <Tile 
@@ -173,6 +189,7 @@ export default function Home() {
                             subTitle="Languages, Frameworks, Stacks, databases."
                             background="skills"
                             height='h-115'
+                            number='3'
                             data={['<b><b>Languages:</b></b> C#, C++, Javascript, Typescript.', '<b><b>Backend Frameworks:</b></b> Node.js (Express), ASP.NET Core WebAPI.', '<b><b>Frontend Frameworks:</b></b> React.js, React Native, .NET MAUI', '<b><b>Databases:</b></b>  PostgresSQL, MongoDB Atlas.', '<b><b>Cloud Platforms:</b></b>  Amazon Web Services RDB, Microsoft Azure ASP NET Core Web API, Vercel Next.js application.']}
                         />
                         <Tile 
@@ -180,6 +197,7 @@ export default function Home() {
                             subTitle="Applications, Databases, Demos, Websites."
                             background="projects"
                             height='h-65'
+                            number='4'
                             data={['<b><b>www.mooseum.online</b></b>', '<b><b>Inventori</b></b> (Link to source code)', '<b><b>Odyssey</b></b> (link to source code)']}
                         />
                         <Tile 
@@ -187,6 +205,7 @@ export default function Home() {
                             subTitle="High school & University."
                             background="education"
                             height='h-95'
+                            number='5'
                             data={['I studied at <b><b>Phumzile Secondary School</b></b> and earned a <b><b>National Senior Certificate</b></b> in January 2018.', 'I went on to study <b><b>Computer Science and Applied Mathematics</b></b> at <b><b>Wits University</b></b> and earned a Bachelor of Science in April 2023.']}
                         />
                         <Tile 
@@ -194,6 +213,7 @@ export default function Home() {
                             subTitle="Where I’ve worked and what I did there."
                             background="experience"
                             height='h-90'
+                            number='6'
                             data={['I worked at <b><b>Nkangala District Municipality</b></b> from July 2024 - June 2026 as an <b><b>Information and Communication Technology</b></b> intern. Rotating in different roles within the department as a service desk operator, assistant to the municipality technician or a reporting analyst.']}
                         />
                         
@@ -202,6 +222,7 @@ export default function Home() {
                             subTitle="For an Interview, discussion or collaboration."
                             background="contact"
                             height='h-45'
+                            number='7'
                             data={['Choose how you want to connect.']}
                         />
                     </Stack>
@@ -212,17 +233,17 @@ export default function Home() {
                 onClose={() => setOpen(false)}
                 className='bg-[#000000]/70!'
             >
-                <DialogTitle className='font-annie! font-bold! text-4xl! bg-[#283618] text-primary!'>
+                <DialogTitle className={`font-annie! text-4xl! [-webkit-text-stroke:1.4px] bg-[url('https://www.transparenttextures.com/patterns/groovepaper.png')] bg-[#283618] text-primary!`}>
                     Welcome to my portfolio!
                 </DialogTitle>
-                <DialogContent className='bg-[#283618]'>
+                <DialogContent className={`bg-[url('https://www.transparenttextures.com/patterns/groovepaper.png')] bg-[#283618]`}>
                     <DialogContentText className='font-urbanist! font-extralight! text-primary!'>
-                        If you want to know something about me, <b><b>flip a card</b></b>.
-                        The arrows are just there to guide you on where to start
-                        and yes, can start with any card you want, Enjoy <b><b>:)</b></b>.
+                        If you want to know something about me, <b><b>flip a card</b></b>. The 
+                        numbers on the cards are just there to guide you on where to start and yes, 
+                        you can start with any card you want, Enjoy <b><b>:)</b></b>.
                 </DialogContentText>
                 </DialogContent>
-                <DialogActions className='bg-[#283618]'>
+                <DialogActions className={`bg-[url('https://www.transparenttextures.com/patterns/groovepaper.png')] bg-[#283618]`}>
                     <Button onClick={() => setOpen(false)} className='font-urbanist! font-semibold! text-[#8ecae6]!'>
                         Got it
                     </Button>
